c60f82c1b7672a32f41f9b7875e4b459d5568012
Joseph Chu <hotcupofjoe2013@gmail.com>
2025-10-11 06:55:46 -0400

    chore: remove all legacy SQLite code from taskModel.js

--- models/taskModel.js
+++ models/taskModel.js
@@ -1,7 +1,10 @@
 // models/taskModel.js
 const connectDB = require('../db');
 
-// No initTaskTable needed for MongoDB
+// MongoDB-based task model functions only
+const connectDB = require('../db');
+const { ObjectId } = require('mongodb');
+
 async function getOpenTasks(workspace_id) {
   const db = await connectDB();
   return db
@@ -49,7 +52,6 @@ async function updateTask(
   priority,
 ) {
   const db = await connectDB();
-  const { ObjectId } = require('mongodb');
   return db.collection('tasks').updateOne(
     { _id: new ObjectId(id), workspace_id },
     {
@@ -67,7 +69,6 @@ async function updateTask(
 
 async function completeTask(workspace_id, id) {
   const db = await connectDB();
-  const { ObjectId } = require('mongodb');
   return db
     .collection('tasks')
     .updateOne(
@@ -78,86 +79,9 @@ async function completeTask(workspace_id, id) {
 
 async function deleteTask(workspace_id, id) {
   const db = await connectDB();
-  const { ObjectId } = require('mongodb');
   return db
     .collection('tasks')
     .deleteOne({ _id: new ObjectId(id), workspace_id });
-}
-
-function getOpenTasks(workspace_id, callback) {
-  db.all(
-    `SELECT * FROM tasks WHERE status = 'open' AND workspace_id = ?`,
-    [workspace_id],
-    callback,
-  );
-}
-
-function createTask(
-  workspace_id,
-  description,
-  assignedUser,
-  dueDate,
-  category,
-  tags,
-  priority,
-  callback,
-) {
-  db.run(
-    `INSERT INTO tasks (workspace_id, description, assigned_user, due_date, category, tags, priority) VALUES (?, ?, ?, ?, ?, ?, ?)`,
-    [
-      workspace_id,
-      description,
-      assignedUser,
-      dueDate,
-      category,
-      tags,
-      priority,
-    ],
-    callback,
-  );
-}
-
-function updateTask(
-  workspace_id,
-  id,
-  newDesc,
-  assignedUser,
-  dueDate,
-  category,
-  tags,
-  priority,
-  callback,
-) {
-  db.run(
-    `UPDATE tasks SET description = ?, assigned_user = ?, due_date = ?, category = ?, tags = ?, priority = ? WHERE id = ? AND workspace_id = ?`,
-    [
-      newDesc,
-      assignedUser,
-      dueDate,
-      category,
-      tags,
-      priority,
-      id,
-      workspace_id,
-    ],
-    callback,
-  );
-}
-
-function completeTask(workspace_id, id, callback) {
-  db.run(
-    `UPDATE tasks SET status = 'complete' WHERE id = ? AND workspace_id = ?`,
-    [id, workspace_id],
-    callback,
-  );
-}
-
-function deleteTask(id, callback) {
-  db.run(
-    `DELETE FROM tasks WHERE id = ? AND workspace_id = ?`,
-    [id, workspace_id],
-    callback,
-  );
 }
 
 module.exports = {
